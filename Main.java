ae1dd2c86773bb9ec156ee07ddd4fe5a0d9d726f
shuvishma <36675563+shuvishma@users.noreply.github.com>
2021-05-12 16:37:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+/**
+ * Main
+ */
+public class Main {
+
+    public static String reverse(String str) {
+        if (str == null || str.length() <= 1) {
+            return str;
+        }
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(reverse(str));
+    }
+}
